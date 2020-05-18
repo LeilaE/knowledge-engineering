310cfe7230b9a20f70525b276d4454fd0110ac79
MilicaCulibrk <milica6.1.97thebest@gmail.com>
2020-05-18 20:23:48 +0200

    additonal tests and inital diagnoses

--- CaseBased/src/similarity/ListSimilarity.java
+++ CaseBased/src/similarity/ListSimilarity.java
@@ -3,17 +3,36 @@ package similarity;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ucm.gaia.jcolibri.exception.NoApplicableSimilarityFunctionException;
 import ucm.gaia.jcolibri.method.retrieve.NNretrieval.similarity.LocalSimilarityFunction;
 
 public class ListSimilarity implements LocalSimilarityFunction {
-	
+
 	@Override
-	public double compute(Object value1, Object value2) throws NoApplicableSimilarityFunctionException {
-		int startSize = ((List) value1).size();
-		((List) value1).retainAll( (List) value2 );
-		return ((List) value1).size() / startSize;
+	public double compute(Object list1, Object list2) throws NoApplicableSimilarityFunctionException {
+		if (list1 == null || list2 == null)
+			return 0;
+		int startSize1 = ((List) list1).size();
+		int startSize2 = ((List) list2).size();
+		double counter = 0;
+
+		for (String element1 : (List<String>) list1) {
+			for (String element2 : (List<String>) list2) {
+				if (element1.equals(element2)) {
+					counter = counter + 1;
+					break;
+				}
+			}
+		}
+
+		if (startSize1 > startSize2) {
+			return counter / startSize1;
+		}else{
+			return counter / startSize2;
+		}
+
 	}
 
 	@Override
@@ -23,9 +42,9 @@ public class ListSimilarity implements LocalSimilarityFunction {
 		return false;
 	}
 
-	
 
 
-	
-	
+
+
+
 }
